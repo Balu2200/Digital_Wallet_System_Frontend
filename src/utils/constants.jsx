72cda911c0fce229b0dd0702029a2c6abef30942
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-02-09 18:59:24 +0530

    added the deployed link

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:1234" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:1234" 
+    : "https://payswift-backend.onrender.com"; 
