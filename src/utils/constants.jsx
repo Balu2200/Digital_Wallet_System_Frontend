ad3f596d4cf7afbd078b62c745d78c8fed77f19d
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-02-09 20:53:44 +0530

    add backend api

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,4 @@
-export const BASE_URL = "https://payswift-backend.onrender.com"; 
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:1234" 
+    : "https://payswift-backend.onrender.com"; 
